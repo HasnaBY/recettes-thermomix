c5534c18d9e4d7ea8e93609c658867b678f9014e
HasnaBY <withlovehasna@gmail.com>
2026-07-28 20:15:56 +0200

    rename in map and robots

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/admin', '/api'],
     },
-    sitemap: 'https://recettes-thermomix.vercel.app/sitemap.xml',
+    sitemap: 'https://www.withlovehasna.com/sitemap.xml',
   }
 }
