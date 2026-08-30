0ae79f2da2e2084e2543b8658600a3632020f10c
HasnaBY <withlovehasna@gmail.com>
2026-08-30 09:45:16 +0200

    czlibrage

--- lib/pdf/MenuPdfDocument.tsx
+++ lib/pdf/MenuPdfDocument.tsx
@@ -6,7 +6,7 @@ import { formatDateRange } from '@/lib/dateHelpers'
 // Recalibré à partir du visuel : la ligne "DU ___ AU ___" est plus haute que le bandeau
 // précédent, et chaque ligne de jour est plus basse et plus grande que l'estimation initiale.
 const DATE_TOP = 22
-const ROW_TOP = [29.5, 42.1, 53.7, 65.3, 76.9]
+const ROW_TOP = [30.5, 42.1, 53.7, 65.3, 76.9]
 const ROW_HEIGHT = 12.5
 const PLAT_LEFT = 24.4
 const PLAT_WIDTH = 32.6
